b90b9fc9259b7ff573bb1d2603f3119248d0e281
CharishmaGajula <charishma.gajula@minfytech.com>
2025-06-23 09:32:06 +0530

    changes in medium

--- vibe_medium/config/db.js
+++ vibe_medium/config/db.js
@@ -1,15 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
 import mongoose from "mongoose";
 
-
-function connect()
-{
-    try{
-        const con=mongoose.connect(process.env.db_url);
-        console.log(`connected`);
-    }
-    catch{
-        console.log("Sorry issue");
-        
-    }
-}
 export default connect;
--- vibe_medium/index.js
+++ vibe_medium/index.js
@@ -1,19 +1,34 @@
-import connect from "./config/db.js";
-import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+import express from "express";
 import authRoutes from './routes/auth.js';
 import viberoutes from './routes/vibes.js'
-
-dotenv.config();
-
 const app=express();
 app.use(express.json());
-connect();
+
+
+const connect = async () => {
+  try {
+    console.log(process.env.db_url);
+    const conn = await mongoose.connect(process.env.db_url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error("❌ DB Connection Failed:", err.message);
+    process.exit(1);
+  }
+};
+connect()
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/vibes',viberoutes);
 
-app.listen(process.env.port,()=>
+app.listen(process.env.PORT,()=>
 {
     console.log("listening at https://localhost:5010/");
 })
+
